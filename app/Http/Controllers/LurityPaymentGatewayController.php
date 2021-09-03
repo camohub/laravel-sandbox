5f735182d62879c15af2899eaa0027da9afc99da
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-09-03 19:36:07 +0200

    LurityPaymentGatewayController::checkout return fix

--- app/Http/Controllers/LurityPaymentGatewayController.php
+++ app/Http/Controllers/LurityPaymentGatewayController.php
@@ -59,10 +59,11 @@ class LurityPaymentGatewayController extends Controller
 		if ($result->success || !is_null($result->transaction))
 		{
 			$transaction = $result->transaction;
-			return redirect()->route('gateway-transaction', ['id' => $transaction->id]);
+			return response()->json(['id' => $transaction->id]);
 		}
 		else
 		{
+
 			$errors = [];
 			foreach($result->errors->deepAll() as $error) $errors[] = "Error: {$error->code}: {$error->message}.";
 
@@ -78,47 +79,17 @@ class LurityPaymentGatewayController extends Controller
 
 		if (in_array($transaction->status, $this->transactionSuccessStatuses))
 		{
-			$result = [];
-			$result['success'] = 'Your test transaction has been successfully processed. See the Braintree API response and try again.';
-
-			$result['transaction'] = [
-				'id' => $transaction->id,
-				'type' => $transaction->type,
-				'amount' => $transaction->amount,
-				'status' => $transaction->status,
-				'createdAt' => $transaction->createdAt->format('Y-m-d H:i:s'),
-				'updatedAt' => $transaction->updatedAt->format('Y-m-d H:i:s'),
-			];
-
-			$result['creditCardDetails'] = [
-				'token' => $transaction->creditCardDetails->token,
-				'bin' => $transaction->creditCardDetails->bin,
-				'last4' => $transaction->creditCardDetails->last4,
-				'cardType' => $transaction->creditCardDetails->cardType,
-				'cardHolderName' => $transaction->creditCardDetails->cardholderName,
-				'expirationDate' => $transaction->creditCardDetails->expirationDate,
-				'customerLocation' => $transaction->creditCardDetails->customerLocation,
-			];
-
-			if( !is_null($transaction->customerDetails->id) )
-			{
-				$result['customerDetail'] = [
-					'id' => $transaction->customerDetails->id,
-					'firstName' => $transaction->customerDetails->firstName,
-					'lastName' => $transaction->customerDetails->lastName,
-					'email' => $transaction->customerDetails->email,
-					'company' => $transaction->customerDetails->company,
-					'website' => $transaction->customerDetails->website,
-					'phone' => $transaction->customerDetails->phone,
-					'fax' => $transaction->customerDetails->fax,
-				];
-			}
-
-			return response()->json(['transactionDetails' => $result]);
+			return response()->json([
+				'success' => 'Your test transaction has been successfully processed. See the Braintree API response and try again.',
+				'transactionDetails' => $transaction,
+			]);
 		}
 		else
 		{
-			return response()->json(['errors' => ["Your test transaction has a status of " . $transaction->status . ". See the Braintree API response and try again."]]);
+			return response()->json([
+				'errors' => ["Transaction status {$transaction->status}. See the Braintree API response and try again."],
+				'transactionDetails' => $transaction,
+			]);
 		}
 	}
 
